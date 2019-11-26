cbf84e84d10444a5af768c5160af42d223d80677
Jeff Chiu <shadow3x3x3@gmail.com>
2019-11-26 16:01:42 +0800

    Use date instead movie time, add sessions

--- internal/pkg/viewshow/movie.go
+++ internal/pkg/viewshow/movie.go
@@ -8,21 +8,28 @@ import (
 )
 
 const (
-	getMoviesURL     = "https://www.vscinemas.com.tw/vsweb/api/GetLstDicMovie"
-	getMoviesTimeURL = "https://www.vscinemas.com.tw/vsweb/api/GetLstDicDate"
+	getMoviesURL        = "https://www.vscinemas.com.tw/vsweb/api/GetLstDicMovie"
+	getMoviesTimeURL    = "https://www.vscinemas.com.tw/vsweb/api/GetLstDicDate"
+	getMoviesSessionURL = "https://www.vscinemas.com.tw/vsweb/api/GetLstDicSession"
 )
 
-// MovieTime represents the time of movie information
-type MovieTime struct {
-	Text      string `json:"strText"`
-	TimeValue string `json:"strValue"`
+// MovieSession represents the providing session of the movie
+type MovieSession struct {
+	Text string `json:"strText"`
+}
+
+// MovieDate represents the providing date of the movie
+type MovieDate struct {
+	Text      string          `json:"strText"`
+	TimeValue string          `json:"strValue"`
+	Sessions  []*MovieSession `json:",omitempty"`
 }
 
 // Movie represents the movie information
 type Movie struct {
-	Name    string       `json:"strText"`
-	ID      string       `json:"strValue"`
-	Session []*MovieTime `json:",omitempty"`
+	Name  string       `json:"strText"`
+	ID    string       `json:"strValue"`
+	Dates []*MovieDate `json:",omitempty"`
 }
 
 // GetAll returns a list of all movies by cinemaID
@@ -40,5 +47,54 @@ func getAllMovie(cinemaID string) ([]*Movie, error) {
 		return nil, err
 	}
 
+	for _, m := range movies {
+		dates, err := getMovieDate(cinemaID, m.ID)
+		if err != nil {
+			continue
+		}
+		m.Dates = dates
+		for _, d := range m.Dates {
+			sessions, err := getMovieSession(cinemaID, m.ID, d.TimeValue)
+			if err != nil {
+				continue
+			}
+			d.Sessions = sessions
+		}
+	}
 	return movies, nil
+}
+
+func getMovieDate(cinemaID, movieID string) ([]*MovieDate, error) {
+	url := fmt.Sprintf("%s?cinema=%s&movie=%s", getMoviesTimeURL, cinemaID, movieID)
+
+	rawString, err := util.GetBody(url)
+	if err != nil {
+		return nil, err
+	}
+
+	movieDates := []*MovieDate{}
+
+	if err := json.Unmarshal(rawString, &movieDates); err != nil {
+		return nil, err
+	}
+
+	return movieDates, nil
+}
+
+func getMovieSession(cinemaID, movieID, timeValue string) ([]*MovieSession, error) {
+	url := fmt.Sprintf("%s?cinema=%s&movie=%s&date=%s", getMoviesSessionURL, cinemaID, movieID, timeValue)
+
+	rawString, err := util.GetBody(url)
+	if err != nil {
+		return nil, err
+	}
+
+	movieSessions := []*MovieSession{}
+
+	if err := json.Unmarshal(rawString, &movieSessions); err != nil {
+		return nil, err
+	}
+
+	return movieSessions, nil
+
 }
